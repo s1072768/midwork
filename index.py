da931df09b7ef8e0001067027419cd0c987f8120
s1072768 <91643192+s1072768@users.noreply.github.com>
2023-11-20 21:15:15 +0800

    Update index.py

--- index.py
+++ index.py
@@ -2,3 +2,10 @@ from flask import Flask, render_template, request
 from datetime import datetime
 
 app = Flask(__name__)
+
+@app.route("/myself")
+def about():
+    return render_template("myself.html")
+
+#if __name__ == "__main__":
+    #app.run()
